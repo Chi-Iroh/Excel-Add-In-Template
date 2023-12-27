c51a577a3f1fb83c1e167a811a7a14d3412fa947
Thomas Sayen <thomas.sayen@groupe-rondy.com>
2023-12-27 16:26:57 +0100

    [FIX] : Excel grid max indices

--- src/excel/utils.ts
+++ src/excel/utils.ts
@@ -7,7 +7,7 @@ import { EXCEL_ROWS_MAX, EXCEL_COLUMNS_MAX } from "./limits";
  * @returns the range
  */
 export function getWholeWorksheetRange(worksheet : Excel.Worksheet) : Excel.Range {
-    const worksheetRange = worksheet.getRange(`A1:${CellLocation.fromIndices(EXCEL_COLUMNS_MAX, EXCEL_ROWS_MAX).stringifyInstanceCoords()}`);
+    const worksheetRange = worksheet.getRange(`A1:${CellLocation.fromIndices(EXCEL_ROWS_MAX - 1, EXCEL_COLUMNS_MAX - 1).stringifyInstanceCoords()}`);
     worksheetRange.load("address");
     return worksheetRange;
 }
